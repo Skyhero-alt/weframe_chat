658f4a5f33f0cf04f10c3be8747493a007ba0dd4
Skyhero-alt <skyhero.code@gmail.com>
2023-01-11 00:25:13 +0530

    feat: 1st iteration of db structure

---
+++ pages/api/conversations.js
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const { PrismaClient } = require("@prisma/client");
+
+const client = new PrismaClient({ log: ["query", "info"] });
+
+export default async function handler(req, res) {
+  console.log(req.body);
+  if (req.method === "POST") {
+    try {
+      const result = await client.Conversations.create({
+        data: {
+          members: [req.body.senderId, req.body.receiverId],
+        },
+      });
+      console.log(result);
+      res.send(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    return 0;
+  }
+}
